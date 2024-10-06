0f7610bd6dc8ad07ffb989c74058619130e6022c
Jeevith-U <jeevith367@gmail.com>
2024-10-06 19:07:39 +0530

    navbar completed

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({name, bgOn}) => {
   return (
-    <button className={`w-32 px-4 py-2 rounded-md text-sm ${bgOn ? "bg-primary text-white" : "text-black" }`}>
+    <button className={`w-32 px-4 py-2 rounded-md text-sm font-medium ${bgOn ? "bg-primary text-white" : "text-primary" }`}>
         {name}
     </button>
   )
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import Button from '../../Components/Button'
+import React from "react";
+import Button from "../../Components/Button";
 
 const Navbar = () => {
   return (
-    <nav className='flex'>
+    <nav className="flex justify-center items-center py-6">
+      <div className="flex justify-center items-center w-10/12">
         {/* Logo */}
-        <div className='flex'>
-            <img src="Icon.svg" alt="" />
-            <img src="Nexcent.svg" alt="" className='ml-2'/>
+        <div className="flex">
+          <img src="Icon.svg" alt="" className="h-6"/>
+          <img src="Nexcent.svg" alt="" className="ml-2 h-6" />
         </div>
 
         {/* Nav links */}
-        <ul className='flex w-full justify-evenly items-center'>
+        <div className="w-7/12 flex justify-center items-center">
+          <ul className="flex w-10/12 justify-evenly items-center text-base">
             <li>Home</li>
             <li>service</li>
             <li>feature</li>
             <li>Product</li>
             <li>Testimonial</li>
             <li>FAQ</li>
-        </ul>
+          </ul>
+        </div>
 
         {/* Call to Action */}
-        <Button name={"Login"} bgOn={false}/>
-        <Button name={"Sing up"} bgOn={true}/>
+        <Button name={"Login"} bgOn={false} />
+        <Button name={"Sing up"} bgOn={true} />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
