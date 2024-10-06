3910892eabf9a700d15b5b1f921119c89ed13686
Jeevith-U <jeevith367@gmail.com>
2024-10-06 20:58:45 +0530

    imported images, created basic stucture for Achievements Section

--- src/Pages/Body/Achievements.jsx
+++ src/Pages/Body/Achievements.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Achievements = () => {
-  return (
-    <div>Achievements</div>
-  )
-}
+    const intro = {
+        title: "Helping a local business reinvent itself",
+        desc: "We reached here with our hard work and dedication"
+    }
 
-export default Achievements
+    const counts = [
+        {
+            icon: "achievements/Icon1.svg",
+            count: "2,245,341",
+            label: "Members"
+        },
+        {
+            icon: "achievements/Icon2.svg",
+            count: "46,328",
+            label: "Clubs"
+        },
+        {
+            icon: "achievements/Icon3.svg",
+            count: "828,867",
+            label: "Event Bookings"
+        },
+        {
+            icon: "achievements/Icon4.svg",
+            count: "1,926,436",
+            label: "Payments"
+        }
+    ]
+  return (
+    <div className="w-full py-6 flex justify-center items-center bg-silver">
+      <div className="w-10/12 px-4 flex justify-center items-center">
+
+      </div>
+    </div>
+  );
+};
+
+export default Achievements;
