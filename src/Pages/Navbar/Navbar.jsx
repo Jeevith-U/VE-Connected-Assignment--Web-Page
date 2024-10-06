59add20e56a399a3171cd738a7d02247de2bce89
Jeevith-U <jeevith367@gmail.com>
2024-10-06 18:27:31 +0530

    creating basic structure for Navbar

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+        // Logo
+        <div>
+            <img src="" alt="" />
+        </div>
+
+        // Nav links
+        <ul>
+            <li>Home</li>
+            <li>service</li>
+            <li>feature</li>
+            <li>Product</li>
+            <li>Testimonial</li>
+            <li>FAQ</li>
+        </ul>
+
+        // Call to Action
+        <button>Login</button>
+        <button>Sign up</button>
+    </nav>
   )
 }
 
