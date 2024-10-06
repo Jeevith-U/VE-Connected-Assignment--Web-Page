2301797ca9a5dc68c7766b91fc82d4d673b2bbb6
Jeevith-U <jeevith367@gmail.com>
2024-10-06 19:34:24 +0530

    completed Hero section, added ilusstration, new colours, etc..,

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,16 @@ export default {
   theme: {
     extend: {},
     colors: {
-      primary: "#28CB8B",
+      primary: "#4CAF4F",
+      primary_light: "#A5D6A7",
       white: "#FFF",
-      black: "#263238"
+      black: "#263238",
+      grey: "#4D4D4D",
+      grey2: "#717171",
+      silver: "#F5F7FA"
+    },
+    height: {
+      home: "29rem"
     }
   },
   plugins: [],
