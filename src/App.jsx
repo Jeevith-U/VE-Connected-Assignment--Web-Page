93c72e078967d1f1c3bb3df9d4b4305b56793a88
Jeevith-U <jeevith367@gmail.com>
2024-10-06 18:29:28 +0530

    fixed issue, removed unreleated imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Nexcent from './Pages/Home/Nexcent'
 import Navbar from './Pages/Navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
